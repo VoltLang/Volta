b6bdc8b111d7b3b5b34e0be0d9bb654def2fc158
Bernard Helyer <b.helyer@gmail.com>
2012-12-09 21:26:06 +1300

    semantic: Look up imports in lookup.

--- src/volt/semantic/lookup.d
+++ src/volt/semantic/lookup.d
@@ -2,16 +2,37 @@ module volt.semantic.lookup;
 
 import ir = volt.ir.ir;
 
+/**
+ * Lookup an identifier in a scope and its parent scopes.
+ * Returns the store or null if no match was found.
+ * 
+ * @todo Take a location.
+ */
 ir.Store lookup(ir.Scope _scope, string name)
 {
-	auto current = _scope;
+	ir.Scope current = _scope, previous = _scope;
 	while (current !is null) {
 		auto store = current.getStore(name);
 		if (store !is null) {
 			return store;
 		}
+		previous = current;
 		current = current.parent;
 	}
+
+	auto asMod = cast(ir.Module) previous.node;
+	assert(asMod !is null);
+
+
+	foreach (importedScope; asMod.importedScopes) {
+		auto store = importedScope.getStore(name);
+		if (store !is null) {
+			return store;
+		}
+	}
+
+	/// @todo Error if we found multiple matches in importedScopes.
+
 	return null;
 }
 
