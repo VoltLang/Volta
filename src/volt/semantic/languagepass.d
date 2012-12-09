bc62cd864b229cad41dcc4e044137eac3be1f631
Jakob Bornecrantz <wallbraker@gmail.com>
2012-12-10 00:17:28 +0100

    util: Breakout common path functions into util

--- src/volt/semantic/languagepass.d
+++ src/volt/semantic/languagepass.d
@@ -1,10 +1,11 @@
 module volt.semantic.languagepass;
 
-import std.file : read, exists;
-import std.random : uniform;
+import std.file : read;
 import std.process : getenv, system;
 
 import ir = volt.ir.ir;
+
+import volt.util.path;
 
 import volt.interfaces;
 
@@ -120,55 +121,4 @@ public:
 			passes ~= new PrintVisitor("Running PrintVisitor:");
 		}
 	}
-}
-
-/**
- * Generate a filename in a temporary directory that doesn't exist.
- *
- * Params:
- *   extension = a string to be appended to the filename. Defaults to an empty string.
- *
- * Returns: an absolute path to a unique (as far as we can tell) filename. 
- */
-string temporaryFilename(string extension = "")
-{
-	version (Windows) {
-		string prefix = getenv("TEMP") ~ '/';
-	} else {
-		string prefix = "/tmp/";
-	}
-
-	string filename;
-	do {
-		filename = randomString(32);
-		filename = prefix ~ filename ~ extension;
-	} while (exists(filename));
-
-	return filename;
-}
-
-/**
- * Generate a random string `length` characters long.
- */
-string randomString(size_t length)
-{
-	auto str = new char[length];
-	foreach (i; 0 .. length) {
-		char c;
-		switch (uniform(0, 3)) {
-			case 0:
-				c = uniform!("[]", char, char)('0', '9');
-				break;
-			case 1:
-				c = uniform!("[]", char, char)('a', 'z');
-				break;
-			case 2:
-				c = uniform!("[]", char, char)('A', 'Z');
-				break;
-			default:
-				assert(false);
-		}
-		str[i] = c;
-	}
-	return str.idup;    
 }
---
+++ src/volt/util/path.d
@@ -0,0 +1,58 @@
+// Copyright © 2012, Jakob Bornecrantz.  All rights reserved.
+// See copyright notice in src/volt/license.d (BOOST ver. 1.0).
+module volt.util.path;
+
+import std.file : read, exists;
+import std.random : uniform;
+
+
+/**
+ * Generate a filename in a temporary directory that doesn't exist.
+ *
+ * Params:
+ *   extension = a string to be appended to the filename. Defaults to an empty string.
+ *
+ * Returns: an absolute path to a unique (as far as we can tell) filename. 
+ */
+string temporaryFilename(string extension = "")
+{
+	version (Windows) {
+		string prefix = getenv("TEMP") ~ '/';
+	} else {
+		string prefix = "/tmp/";
+	}
+
+	string filename;
+	do {
+		filename = randomString(32);
+		filename = prefix ~ filename ~ extension;
+	} while (exists(filename));
+
+	return filename;
+}
+
+/**
+ * Generate a random string `length` characters long.
+ */
+string randomString(size_t length)
+{
+	auto str = new char[length];
+	foreach (i; 0 .. length) {
+		char c;
+		switch (uniform(0, 3)) {
+			case 0:
+				c = uniform!("[]", char, char)('0', '9');
+				break;
+			case 1:
+				c = uniform!("[]", char, char)('a', 'z');
+				break;
+			case 2:
+				c = uniform!("[]", char, char)('A', 'Z');
+				break;
+			default:
+				assert(false);
+		}
+		str[i] = c;
+	}
+	return str.idup;    
+}
