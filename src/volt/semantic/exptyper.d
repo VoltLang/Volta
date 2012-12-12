0dd8f764d3304738ed43bd33d09122c102989d7a
Bernard Helyer <b.helyer@gmail.com>
2012-12-13 11:12:28 +1300

    semantic: For now, be more restrictive about type range checking.

--- src/volt/semantic/exptyper.d
+++ src/volt/semantic/exptyper.d
@@ -69,6 +69,23 @@ bool isOkayForPointerArithmetic(ir.PrimitiveType.Kind kind)
 	}
 }
 
+bool isIntegral(ir.PrimitiveType.Kind kind)
+{
+	switch (kind) with (ir.PrimitiveType.Kind) {
+		case Byte:
+		case Ubyte:
+		case Short:
+		case Ushort:
+		case Int:
+		case Uint:
+		case Long:
+		case Ulong:
+			return true;
+		default:
+			return false;
+	}
+}
+
 bool fitsInPrimitive(ir.PrimitiveType t, ir.Exp e)
 {
 	if (e.nodeType != ir.NodeType.Constant) {
@@ -77,52 +94,38 @@ bool fitsInPrimitive(ir.PrimitiveType t, ir.Exp e)
 	auto asConstant = cast(ir.Constant) e;
 	assert(asConstant !is null);
 
-	long l;
-	try {
-		l = to!long(asConstant.value);
-	} catch (Throwable t) {
-		return false;
-	}
-
-	switch (t.type) with (ir.PrimitiveType.Kind) {
-	case Ubyte:
-		return l >= ubyte.min && l <= ubyte.max;
-	case Byte:
-		return l >= byte.min && l <= byte.max;
-	case Ushort:
-		return l >= ushort.min && l <= ushort.max;
-	case Short:
-		return l >= short.min && l <= short.max;
-	case Uint:
-		return l >= uint.min && l <= uint.max;
-	case Int:
-		return l >= int.min && l <= int.max;
-	case Long:
-		return true;
-	case Ulong:
+	if (isIntegral(t.type)) {
+		long l;
 		try {
-			ulong ul = to!ulong(asConstant.value);
-		} catch (Throwable) {
+			l = to!long(asConstant.value);
+		} catch (Throwable t) {
 			return false;
 		}
-		return true;
-	case Float:
-		float f;
-		try {
-			f = to!float(asConstant.value);
-		} catch (Throwable) {
+		switch (t.type) with (ir.PrimitiveType.Kind) {
+		case Ubyte:
+			return l >= ubyte.min && l <= ubyte.max;
+		case Byte:
+			return l >= byte.min && l <= byte.max;
+		case Ushort:
+			return l >= ushort.min && l <= ushort.max;
+		case Short:
+			return l >= short.min && l <= short.max;
+		case Uint:
+			return l >= uint.min && l <= uint.max;
+		case Int:
+			return l >= int.min && l <= int.max;
+		case Long:
+			return true;
+		case Ulong:
+			return false;
+		case Float:
+			return l >= float.min && l <= float.max;
+		case Double:
+			return l >= double.min && l <= double.max;
+		default:
 			return false;
 		}
-		return true;
-	case Double:
-		double d;
-		try {
-			d = to!double(asConstant.value);
-		} catch (Throwable) {
-			return false;
-		}
-		return true;
-	default:
+	} else {
 		return false;
 	}
 }
