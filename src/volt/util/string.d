01f7a06add4299858215e6476ed2cad6069cc161
Bernard Helyer <b.helyer@gmail.com>
2012-12-14 09:18:05 +1300

    util: Handle unicode codepoint specifications.

--- src/volt/util/string.d
+++ src/volt/util/string.d
@@ -27,9 +27,41 @@ void[] unescape(T)(Location location, const T[] s)
 {
 	T[] output;
 
-	bool escaping, hexing;
+	bool escaping, hexing, unicoding;
 	dchar[] hexchars;
 	foreach (c; s) {
+		// \uXXXX
+		if (unicoding) {
+			if (!isHex(c)) {
+				if (hexchars.length == 4) {
+					ushort i;
+					try {
+						i = parse!ushort(hexchars, 16);
+					} catch (ConvException) {
+						throw new CompilerError(location, "bad unicode codepoint specification.");
+					}
+					encode(output, i);
+					unicoding = false;
+					continue;
+				} else if (hexchars.length == 8) {
+					uint i;
+					try {
+						i = parse!uint(hexchars, 16);
+					} catch (ConvException) {
+						throw new CompilerError(location, "bad unicode codepoint specification.");
+					}
+					encode(output, i);
+					unicoding = false;
+					continue;
+				} else { 
+					throw new CompilerError(location, "bad unicode codepoint specification.");
+				}
+			}
+			hexchars ~= c;
+			continue;
+		}
+
+		// \xXX
 		if (hexing) {
 			if (!isHex(c)) {
 				throw new CompilerError(location, "bad hex digit.");
@@ -46,6 +78,8 @@ void[] unescape(T)(Location location, const T[] s)
 			}
 			continue;
 		}
+
+		// \X
 		if (escaping) {
 			switch (c) {
 				case '\'': encode(output, '\''); break;
@@ -64,14 +98,17 @@ void[] unescape(T)(Location location, const T[] s)
 					hexing = true;
 					hexchars.length = 0;
 					continue;
+				case 'u', 'U':
+					unicoding = true;
+					hexchars.length = 0;
+					continue;
+				// @todo Named character entities. http://www.w3.org/TR/html5/named-character-references.html
 				default:
 					throw new CompilerError(location, "bad escape.");
 			}
 			escaping = false;
 			continue;
 		}
-
-		// @todo Named character entities. http://www.w3.org/TR/html5/named-character-references.html
 
 		if (c == '\\') {
 			escaping = true;
