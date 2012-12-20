d04cafff6e27118dfab681780574f9f6ec79c02b
Jakob Bornecrantz <wallbraker@gmail.com>
2012-12-20 04:48:56 +0100

    rt: Document allocDg behavoir

--- rt/src/object.d
+++ rt/src/object.d
@@ -2,8 +2,47 @@
 // See copyright notice in src/volt/license.d (BOOST ver. 1.0).
 module object;
 
-// This is all up in the air.
-alias AllocDg = void delegate(uint size);
+/**
+ * This is all up in the air. But here is how its intended to work.
+ *
+ * @param typeinfo The type to which we should allocate storage for.
+ * @param count the number of elements in a array, zero if just the type.
+ *
+ * The count logic is a bit odd. If count is zero we are allocating the
+ * storage for just the type alone, if count is greater then one we are
+ * allocating the storage an array of that type. Here how it is done,
+ * Thow following shows what happends for some cases.
+ *
+ * For primitive types:
+ * int* ptr = new int;
+ * int* ptr = allocDg(typeid(int), 0);
+ * // Alloc size == int.sizeof == 4
+ *
+ * While for arrays:
+ * int[] arr; arr.length = 5;
+ * int[] arr; { arr.ptr = allocDg(typeid(int), 5); arr.length = 5 }
+ * // Alloc size == int.sizeof * 5 == 20
+ *
+ * Classes are weird, tho in the normal case not so much but notice the -1.
+ * Clazz foo = new Clazz();
+ * Clazz foo = allocDg(typeid(Clazz), cast(size_t)-1);
+ * // Alloc size == Clazz.storage.sizeof
+ *
+ * Here its where it gets weird: this is because classes are references.
+ * Clazz foo = new Clazz;
+ * Clazz foo = allocDg(typeid(Clazz), 0);
+ * // Alloc size == (void*).sizeof
+ *
+ * And going from that this makes sense.
+ * Clazz[] arr; arr.length = 3;
+ * Clazz[] arr; { arr.ptr = allocDg(typeid(Clazz), 3); arr.length = 3 }
+ * // Alloc size == (void*).sizeof * 3
+ *
+ * alias AllocDg = void* delegate(TypeInfo typeinfo, uint count);
+ * Temporary until we have typeinfos.
+ */
+alias AllocDg = void* delegate(uint size);
+
 
 local AllocDg allocDg;
 
