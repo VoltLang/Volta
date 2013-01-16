bede3415470bf64a722e647984664e79ea50267b
Jakob Bornecrantz <wallbraker@gmail.com>
2013-01-16 23:01:57 +0100

    test: Tests for slicing

---
+++ test/arrays/test_003.d
@@ -0,0 +1,13 @@
+//T compiles:yes
+//T retval:4
+// Test slicing.
+
+module test_003;
+
+int main()
+{
+	char[] str = new char[6];
+	char[] otherStr = str[0 .. 4];
+
+	return cast(int)otherStr.length;
+}
---
+++ test/arrays/test_004.d
@@ -0,0 +1,24 @@
+//T compiles:yes
+//T retval:42
+// Test slicing.
+
+module test_004;
+
+int sumArray(char[] str)
+{
+	uint sum;
+	for (uint i; i < str.length; i++) {
+		sum = sum + str[i];
+	}
+	return cast(int)sum;
+}
+
+int main()
+{
+	int index = 3;
+	int val = sumArray("TheVoltIsAwesome"[index .. 7]);
+	if (val == 421)
+		return 42;
+	else
+		return 0;
+}
