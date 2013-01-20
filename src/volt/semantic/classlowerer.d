a4eba8c3ab39c37908443ccbc4dbaad6a7b4a6ad
Jakob Bornecrantz <wallbraker@gmail.com>
2013-01-20 14:31:36 +0100

    semantic: Add missing copyright headers

--- src/volt/semantic/classlowerer.d
+++ src/volt/semantic/classlowerer.d
@@ -1,3 +1,6 @@
+// Copyright © 2012-2013, Bernard Helyer.  All rights reserved.
+// Copyright © 2013, Jakob Bornecrantz.  All rights reserved.
+// See copyright notice in src/volt/license.d (BOOST ver. 1.0).
 module volt.semantic.classlowerer;
 
 import std.array : insertInPlace;
