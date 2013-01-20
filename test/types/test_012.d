26896b6ce734766bc344cc7061e282730f219ec0
Bernard Helyer <b.helyer@gmail.com>
2013-01-20 14:47:21 +1300

    test: Add types 12, testing typeof thoroughly.

---
+++ test/types/test_012.d
@@ -0,0 +1,11 @@
+//T compiles:yes
+//T retval:42
+// Tests correct functioning of typeof.
+module test_012;
+
+int main()
+{
+    int i = 1;
+    typeof(i++) AnInteger = 41;  // i should not mutate.
+    return AnInteger + i;
+}
