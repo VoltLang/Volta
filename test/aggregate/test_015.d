5d57e56547b2a4422ee3e6fde1aa652372119e85
Bernard Helyer <b.helyer@gmail.com>
2013-01-21 19:42:13 +1300

    test: Add mysteriously failing aggregate 15.

---
+++ test/aggregate/test_015.d
@@ -0,0 +1,25 @@
+//T compiles:yes
+//T retval:42
+//T has-passed:no
+module test_015;
+
+class S {
+    int mX;
+
+    int y()
+    {
+        return mX;
+    }
+    void x(int _x)
+    {
+        mX = _x;
+        return;
+    }
+}
+
+int main()
+{
+    S s = new S();
+    s.x(42);
+    return s.y();
+}
