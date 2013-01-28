b98ab91eb77f014da0aee240fa1375bd1e58a5dc
Bernard Helyer <b.helyer@gmail.com>
2013-01-28 16:48:06 +1300

    test: Add bugs 9 testing @property functions in weird places.

---
+++ test/bugs/test_009.v
@@ -0,0 +1,27 @@
+//T compiles:yes
+//T retval:6
+module test_009;
+
+@property int four() { return 4; }
+
+class PlusTwoFactory
+{
+    int mX;
+
+    this(int x)
+    {
+        mX = x + 2;
+        return;
+    }
+
+    int get()
+    {
+        return mX;
+    }
+}
+
+int main()
+{
+    auto factory = new PlusTwoFactory(four);
+    return factory.get();
+}
