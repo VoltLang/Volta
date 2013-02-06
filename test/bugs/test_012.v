b2400cbd92b147a0de3dc1f0bd8684d89f112ef2
Jakob Bornecrantz <wallbraker@gmail.com>
2013-02-06 12:45:56 +0100

    test: Add bug for local var shadowing member var

---
+++ test/bugs/test_012.v
@@ -0,0 +1,27 @@
+//T compiles:yes
+//T retval:42
+//T has-passed:no
+// Local variable shadowing member variable.
+module test_012;
+
+class Clazz
+{
+	int g;
+
+	this(int g)
+	{
+		this.g = g;
+		return;
+	}
+
+	int func(int g)
+	{
+		return this.g + g;
+	}
+}
+
+int main()
+{
+	auto t = new Clazz(1);
+	return t.g + t.func(42);
+}
