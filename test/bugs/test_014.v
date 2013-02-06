c9055fe779ae3d28d8a75892b59adb342d222770
Jakob Bornecrantz <wallbraker@gmail.com>
2013-02-06 19:57:54 +0100

    test: Add segfaulting decl order test

---
+++ test/bugs/test_014.v
@@ -0,0 +1,22 @@
+//T compiles:yes
+//T retval:0
+//T has-passed:no
+// Segfault due to declaration order, and null.
+module test_014;
+
+Foo func()
+{
+	return null; // This is what triggers it.
+}
+
+// If this class is defined the function it works
+// But declared after as it is now, it causes a segfault.
+class Foo
+{
+	this() { return; }
+}
+
+int main()
+{
+	return 0;
+}
