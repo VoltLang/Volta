d66d3cc3f205793f9c1fc6592b13325b7212a738
Bernard Helyer <b.helyer@gmail.com>
2013-02-10 00:44:00 +1300

    test: Add aggregate 18 testing super ctor calls.

---
+++ test/aggregate/test_018.v
@@ -0,0 +1,34 @@
+//T compiles:yes
+//T retval:42
+module test_018;
+
+class A
+{
+    int mX;
+
+    this(int x)
+    {
+        mX = x;
+        return;
+    }
+
+    int getX()
+    {
+        return mX;
+    }
+}
+
+class B : A
+{
+    this(int x)
+    {
+        super(x + 1);
+        return;
+    }
+}
+
+int main()
+{
+    auto b = new B(41);
+    return b.getX();
+}
