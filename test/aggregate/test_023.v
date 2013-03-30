f3543ac028a9bbcb10df42b3b9540e2909284b33
Jakob Bornecrantz <wallbraker@gmail.com>
2013-03-30 17:25:28 +0100

    test: Add Union

---
+++ test/aggregate/test_023.v
@@ -0,0 +1,22 @@
+//T compiles:yes
+//T retval:42
+// Union test.
+module test_024;
+
+union Test
+{
+	int i;
+	uint u;
+	long l;
+}
+
+int main()
+{
+	auto tid = typeid(Test);
+
+	Test t;
+	t.i = -1;
+	if (tid.size == 8 && t.u == cast(uint)-1)
+		return 42;
+	return 0;
+}
