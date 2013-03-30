a0769ba1430772c1f4a53044e93c8b137a789ac9
Jakob Bornecrantz <wallbraker@gmail.com>
2013-03-30 20:13:33 +0100

    rt: Add ptrdiff_t alias

--- rt/src/defaultsymbols.v
+++ rt/src/defaultsymbols.v
@@ -2,8 +2,10 @@ module defaultsymbols;
 
 version (V_P64) {
 	alias size_t = ulong;
+	alias ptrdiff_t = long;
 } else {
 	alias size_t = uint;
+	alias ptrdiff_t = int;
 }
 
 alias string = immutable(char)[];
