58dffa6d4f49365aa34542e4d3326d6a0797204c
Jakob Bornecrantz <wallbraker@gmail.com>
2013-04-20 20:40:47 +0200

    llvm: Add support for OSX 10.7.0

--- src/volt/llvm/state.d
+++ src/volt/llvm/state.d
@@ -261,7 +261,9 @@ protected:
 			LLVMSetDataLayout(mod, layoutList[mSettings.arch]);
 			break;
 		case OSX:
-			assert(false);
+			LLVMSetTarget(mod, targetOSXList[mSettings.arch]);
+			LLVMSetDataLayout(mod, layoutOSXList[mSettings.arch]);
+			break;
 		}
 	}
 }
@@ -279,15 +281,33 @@ string[] targetMinGWList = [
 ];
 
 /**
- * This is what clang uses for linux.
+ * This is what clang uses for Linux.
  */
 string[] targetLinuxList = [
 	"i386-unknown-linux-gnu",
 	"x86_64-unknown-linux-gnu",
 ];
 
-// Shared between platforms.
+/**
+ * This is what clang uses for OSX.
+ */
+string[] targetOSXList = [
+	"i386-apple-macosx10.7.0",
+	"x86_64-apple-macosx10.7.0",
+];
+
+/**
+ * Shared between windows and linux platforms.
+ */
 string[] layoutList = [
 	"e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:32:64-f32:32:32-f64:32:64-v64:64:64-v128:128:128-a0:0:64-f80:32:32-n8:16:32-S128",
+	"e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128-n8:16:32:64-S128",
+];
+
+/**
+ * OSX layouts grabbed from clang.
+ */
+string[] layoutOSXList = [
+	"e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:32:64-f32:32:32-f64:32:64-v64:64:64-v128:128:128-a0:0:64-f80:128:128-n8:16:32-S128",
 	"e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128-n8:16:32:64-S128",
 ];
