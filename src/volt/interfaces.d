d6dda46bc67348661771cc96ce9a24255736360c
Bernard Helyer <b.helyer@gmail.com>
2014-02-04 02:36:33 +1300

    volt: Update copyright date on interfaces.

--- src/volt/interfaces.d
+++ src/volt/interfaces.d
@@ -1,4 +1,4 @@
-// Copyright © 2012, Jakob Bornecrantz.  All rights reserved.
+// Copyright © 2012-2014, Jakob Bornecrantz.  All rights reserved.
 // See copyright notice in src/volt/license.d (BOOST ver. 1.0).
 module volt.interfaces;
 
