ae351322921d68bc59e0a3d0061b0673333d3099
Jakob Bornecrantz <wallbraker@gmail.com>
2014-05-25 14:46:56 +0200

    semantic: Correct size for AAs

--- src/volt/semantic/classify.d
+++ src/volt/semantic/classify.d
@@ -91,7 +91,7 @@ int size(Location location, LanguagePass lp, ir.Node node)
 	case ArrayType:
 		return lp.settings.isVersionSet("V_P64") ? 16 : 8;
 	case AAType:
-		return lp.settings.isVersionSet("V_P64") ? 16 : 8;
+		return lp.settings.isVersionSet("V_P64") ? 8 : 4;
 	case TypeReference:
 		auto asTR = cast(ir.TypeReference) node;
 		assert(asTR !is null);
