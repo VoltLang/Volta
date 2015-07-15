cc60a6f6d0f2b43b3b576433efb56fbd4c214d6e
Jakob Bornecrantz <wallbraker@gmail.com>
2015-07-15 14:52:53 +0200

    parser: Add copyright

--- src/volt/parser/stream.d
+++ src/volt/parser/stream.d
@@ -1,3 +1,5 @@
+// Copyright © 2015, Jakob Bornecrantz.  All rights reserved.
+// See copyright notice in src/volt/license.d (BOOST ver. 1.0).
 module volt.parser.stream;
 
 import volt.token.token : Token;
