edf84f2eae3f288ecb1ba95c453cf2c865285cf8
Jakob Bornecrantz <wallbraker@gmail.com>
2015-11-06 14:10:40 +0100

    watt: Export splitLines

--- src/watt/text/string.d
+++ src/watt/text/string.d
@@ -1,5 +1,5 @@
 module watt.text.string;
 
 import std.algorithm : endsWith;
-import std.string : indexOf, strip, split;
+import std.string : indexOf, strip, split, splitLines;
 import std.array : replace;
