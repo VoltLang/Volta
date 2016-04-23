d775452f5b1497b7e7955791f8c56bede0e757b4
Jakob Bornecrantz <wallbraker@gmail.com>
2016-04-23 17:02:28 +0200

    watt: Add StringSink from Watt

---
+++ src/watt/text/sink.d
@@ -0,0 +1,64 @@
+// Copyright © 2015, Jakob Bornecrantz.  All rights reserved.
+// See copyright notice in src/watt/licence.volt (BOOST ver 1.0).
+module watt.text.sink;
+
+
+/// The one true sink definition.
+alias Sink = scope void delegate(SinkArg);
+
+/// The argument to the one true sink.
+alias SinkArg = scope const(char)[];
+
+/// A sink to create long strings.
+struct StringSink
+{
+private:
+	char[] mArr;
+	size_t mLength;
+
+	enum size_t minSize = 16;
+	enum size_t maxSize = 2048;
+
+public:
+	void sink(SinkArg str)
+	{
+		auto newSize = str.length + mLength;
+		if (newSize <= mArr.length) {
+			mArr[mLength .. newSize] = str[];
+			mLength = newSize;
+			return;
+		}
+
+		auto allocSize = mArr.length;
+		while (allocSize < newSize) {
+			if (allocSize < minSize) {
+				allocSize = minSize;
+			} else if (allocSize >= maxSize) {
+				allocSize += maxSize;
+			} else {
+				allocSize = allocSize * 2;
+			}
+		}
+
+		auto n = new char[](newSize + 256);
+		n[0 .. mLength] = mArr[0 .. mLength];
+		n[mLength .. newSize] = str[];
+		mLength = newSize;
+		mArr = n;
+	}
+
+	string toString()
+	{
+		version (Volt) {
+			return new string(mArr[0 .. mLength]);
+		} else {
+			return mArr[0 .. mLength].idup;
+		}
+	}
+
+	void reset()
+	{
+		mArr = [];
+		mLength = 0;
+	}
+}
