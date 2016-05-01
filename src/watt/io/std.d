f38c7ba0f3b720efd583fab0756c510bd6cecfec
Jakob Bornecrantz <wallbraker@gmail.com>
2016-05-01 13:25:30 +0200

    watt: DMD now finally checks private imports correctly
    Only took them how many years?

--- src/watt/io/std.d
+++ src/watt/io/std.d
@@ -1,5 +1,6 @@
 module watt.io.std;
 
+public:
 import std.stdio : writef, writefln;
 import undead.cstream : output = dout;
 import undead.cstream : error = derr;
--- src/watt/text/string.d
+++ src/watt/text/string.d
@@ -1,5 +1,6 @@
 module watt.text.string;
 
+public:
 import std.algorithm : endsWith;
 import std.string : indexOf, strip, split, splitLines;
 import std.array : replace;
