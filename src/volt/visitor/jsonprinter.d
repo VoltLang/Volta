86006f1c67ca88f9f14da6531f06c76efcac3c73
Bernard Helyer <b.helyer@gmail.com>
2016-08-06 04:26:06 +1200

    visitor: Don't try and print types for incomplete aliases.

--- src/volt/visitor/jsonprinter.d
+++ src/volt/visitor/jsonprinter.d
@@ -100,6 +100,9 @@ public:
 
 	override Status enter(ir.Alias a)
 	{
+		if (a.type is null) {
+			return ContinueParent;
+		}
 		auto name = a.name;
 		startObject();
 		writeNamedTyped("alias", name, a.docComment, a.type);
