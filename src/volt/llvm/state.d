24f929cf3bdd962ec5c8084c7068ce101ea3aa5d
Jakob Bornecrantz <wallbraker@gmail.com>
2016-10-07 13:44:38 +0200

    llvm: Fix triple on linux

--- src/volt/llvm/state.d
+++ src/volt/llvm/state.d
@@ -610,8 +610,8 @@ static string[][] tripleList = [
 	 * This is what clang uses for Linux.
 	 */
 	[
-		"i386-unknown-linux-gnu",
-		"x86_64-unknown-linux-gnu",
+		"i386-pc-linux-gnu",
+		"x86_64-pc-linux-gnu",
 		null,
 	],
 
