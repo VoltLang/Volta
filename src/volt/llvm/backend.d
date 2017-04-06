26261be90777c4416d9de98ea4573f6aad05ef27
Jakob Bornecrantz <wallbraker@gmail.com>
2017-04-06 20:57:04 +0200

    llvm: Update layout strings

--- src/volt/llvm/backend.d
+++ src/volt/llvm/backend.d
@@ -347,7 +347,7 @@ static string[][] layoutList = [
 		null,
 	],
 	[ // MSVC
-		layoutWinLinux32,
+		null,
 		layoutWinLinux64,
 		null,
 	],
@@ -369,21 +369,12 @@ static string[][] layoutList = [
 ];
 
 /**
- * Shared between windows and linux platforms.
+ * Layout strings grabbed from clang.
  */
-enum string layoutWinLinux32 = "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:32:64-f32:32:32-f64:32:64-v64:64:64-v128:128:128-a0:0:64-f80:32:32-n8:16:32-S128";
-enum string layoutWinLinux64 = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128-n8:16:32:64-S128";
-
-/**
- * OSX layouts grabbed from clang.
- */
-enum string layoutOSX32 = "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:32:64-f32:32:32-f64:32:64-v64:64:64-v128:128:128-a0:0:64-f80:128:128-n8:16:32-S128";
-enum string layoutOSX64 = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128-n8:16:32:64-S128";
-
-/**
- * The layout that emscripten uses.
- */
-enum string layoutEmscripten = "e-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-p:32:32:32-v128:32:32";
+enum string layoutWinLinux32 = "e-m:e-p:32:32-f64:32:64-f80:32-n8:16:32-S128";
+enum string layoutWinLinux64 = "e-m:e-i64:64-f80:128-n8:16:32:64-S128";
+enum string layoutOSX32 = "e-m:o-p:32:32-f64:32:64-f80:128-n8:16:32-S128";
+enum string layoutOSX64 = "e-m:o-i64:64-f80:128-n8:16:32:64-S128";
 
 /**
  * Bare metal layout, grabbed from clang with target "X-pc-none-elf".
