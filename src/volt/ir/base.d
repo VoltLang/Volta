d59a8c44e6df830e1f5107b47642cb5d6cb27c73
Jakob Bornecrantz <wallbraker@gmail.com>
2017-04-19 12:02:32 +0200

    ir: Add to*Fast helpers on Node

--- src/volt/ir/base.d
+++ src/volt/ir/base.d
@@ -9,6 +9,11 @@ import watt.text.sink : StringSink;
 public import volt.token.location : Location;
 public import volt.token.token : Token, TokenType;
 
+import volt.ir.type;
+import volt.ir.toplevel;
+import volt.ir.statement;
+import volt.ir.templates;
+import volt.ir.expression;
 import volt.ir.declaration;
 
 
@@ -227,6 +232,112 @@ protected:
 		this(nt);  // Setup uniqueId.
 		this.loc = old.loc;
 	}
+
+public:
+	// Base
+	final Identifier toIdentifierFast() { return cast(Identifier)cast(void*)this; }
+	final QualifiedName toQualifiedNameFast() { return cast(QualifiedName)cast(void*)this; }
+
+	// Toplevel
+	final Module toModuleFast() { return cast(Module)cast(void*)this; }
+	final TopLevelBlock toTopLevelBlockFast() { return cast(TopLevelBlock)cast(void*)this; }
+	final Import toImportFast() { return cast(Import)cast(void*)this; }
+	final Unittest toUnittestFast() { return cast(Unittest)cast(void*)this; }
+	final Struct toStructFast() { return cast(Struct)cast(void*)this; }
+	final Class toClassFast() { return cast(Class)cast(void*)this; }
+	final _Interface toInterfaceFast() { return cast(_Interface)cast(void*)this; }
+	final Union toUnionFast() { return cast(Union)cast(void*)this; }
+	final Enum toEnumFast() { return cast(Enum)cast(void*)this; }
+	final Attribute toAttributeFast() { return cast(Attribute)cast(void*)this; }
+	final Condition toConditionFast() { return cast(Condition)cast(void*)this; }
+	final ConditionTopLevel toConditionTopLevelFast() { return cast(ConditionTopLevel)cast(void*)this; }
+	final MixinFunction toMixinFunctionFast() { return cast(MixinFunction)cast(void*)this; }
+	final MixinTemplate toMixinTemplateFast() { return cast(MixinTemplate)cast(void*)this; }
+
+	// Type
+	final PrimitiveType toPrimitiveTypeFast() { return cast(PrimitiveType)cast(void*)this; }
+	final TypeReference toTypeReferenceFast() { return cast(TypeReference)cast(void*)this; }
+	final PointerType toPointerTypeFast() { return cast(PointerType)cast(void*)this; }
+	final ArrayType toArrayTypeFast() { return cast(ArrayType)cast(void*)this; }
+	final AmbiguousArrayType toAmbiguousArrayTypeFast() { return cast(AmbiguousArrayType)cast(void*)this; }
+	final StaticArrayType toStaticArrayTypeFast() { return cast(StaticArrayType)cast(void*)this; }
+	final AAType toAATypeFast() { return cast(AAType)cast(void*)this; }
+	final AAPair toAAPairFast() { return cast(AAPair)cast(void*)this; }
+	final FunctionType toFunctionTypeFast() { return cast(FunctionType)cast(void*)this; }
+	final DelegateType toDelegateTypeFast() { return cast(DelegateType)cast(void*)this; }
+	final FunctionSetType toFunctionSetTypeFast() { return cast(FunctionSetType)cast(void*)this; }
+	final StorageType toStorageTypeFast() { return cast(StorageType)cast(void*)this; }
+
+	// Declaration
+	final FunctionSet toFunctionSetFast() { return cast(FunctionSet)cast(void*)this; }
+	final Variable toVariableFast() { return cast(Variable)cast(void*)this; }
+	final Alias toAliasFast() { return cast(Alias)cast(void*)this; }
+	final Function toFunctionFast() { return cast(Function)cast(void*)this; }
+	final FunctionParam toFunctionParamFast() { return cast(FunctionParam)cast(void*)this; }
+	final TypeOf toTypeOfFast() { return cast(TypeOf)cast(void*)this; }
+	final NullType toNullTypeFast() { return cast(NullType)cast(void*)this; }
+	final EnumDeclaration toEnumDeclarationFast() { return cast(EnumDeclaration)cast(void*)this; }
+	final AutoType toAutoTypeFast() { return cast(AutoType)cast(void*)this; }
+	final NoType toNoTypeFast() { return cast(NoType)cast(void*)this; }
+
+	// Statements
+	final ReturnStatement toReturnStatementFast() { return cast(ReturnStatement)cast(void*)this; }
+	final BlockStatement toBlockStatementFast() { return cast(BlockStatement)cast(void*)this; }
+	final AsmStatement toAsmStatementFast() { return cast(AsmStatement)cast(void*)this; }
+	final IfStatement toIfStatementFast() { return cast(IfStatement)cast(void*)this; }
+	final WhileStatement toWhileStatementFast() { return cast(WhileStatement)cast(void*)this; }
+	final DoStatement toDoStatementFast() { return cast(DoStatement)cast(void*)this; }
+	final ForStatement toForStatementFast() { return cast(ForStatement)cast(void*)this; }
+	final ForeachStatement toForeachStatementFast() { return cast(ForeachStatement)cast(void*)this; }
+	final LabelStatement toLabelStatementFast() { return cast(LabelStatement)cast(void*)this; }
+	final ExpStatement toExpStatementFast() { return cast(ExpStatement)cast(void*)this; }
+	final SwitchStatement toSwitchStatementFast() { return cast(SwitchStatement)cast(void*)this; }
+	final SwitchCase toSwitchCaseFast() { return cast(SwitchCase)cast(void*)this; }
+	final ContinueStatement toContinueStatementFast() { return cast(ContinueStatement)cast(void*)this; }
+	final BreakStatement toBreakStatementFast() { return cast(BreakStatement)cast(void*)this; }
+	final GotoStatement toGotoStatementFast() { return cast(GotoStatement)cast(void*)this; }
+	final WithStatement toWithStatementFast() { return cast(WithStatement)cast(void*)this; }
+	final SynchronizedStatement toSynchronizedStatementFast() { return cast(SynchronizedStatement)cast(void*)this; }
+	final TryStatement toTryStatementFast() { return cast(TryStatement)cast(void*)this; }
+	final ThrowStatement toThrowStatementFast() { return cast(ThrowStatement)cast(void*)this; }
+	final ScopeStatement toScopeStatementFast() { return cast(ScopeStatement)cast(void*)this; }
+	final PragmaStatement toPragmaStatementFast() { return cast(PragmaStatement)cast(void*)this; }
+	final ConditionStatement toConditionStatementFast() { return cast(ConditionStatement)cast(void*)this; }
+	final MixinStatement toMixinStatementFast() { return cast(MixinStatement)cast(void*)this; }
+	final AssertStatement toAssertStatementFast() { return cast(AssertStatement)cast(void*)this; }
+
+	// Expression
+	final Constant toConstantFast() { return cast(Constant)cast(void*)this; }
+	final BinOp toBinOpFast() { return cast(BinOp)cast(void*)this; }
+	final Ternary toTernaryFast() { return cast(Ternary)cast(void*)this; }
+	final Unary toUnaryFast() { return cast(Unary)cast(void*)this; }
+	final Postfix toPostfixFast() { return cast(Postfix)cast(void*)this; }
+	final ArrayLiteral toArrayLiteralFast() { return cast(ArrayLiteral)cast(void*)this; }
+	final AssocArray toAssocArrayFast() { return cast(AssocArray)cast(void*)this; }
+	final IdentifierExp toIdentifierExpFast() { return cast(IdentifierExp)cast(void*)this; }
+	final Assert toAssertFast() { return cast(Assert)cast(void*)this; }
+	final StringImport toStringImportFast() { return cast(StringImport)cast(void*)this; }
+	final Typeid toTypeidFast() { return cast(Typeid)cast(void*)this; }
+	final IsExp toIsExpFast() { return cast(IsExp)cast(void*)this; }
+	final FunctionLiteral toFunctionLiteralFast() { return cast(FunctionLiteral)cast(void*)this; }
+	final ExpReference toExpReferenceFast() { return cast(ExpReference)cast(void*)this; }
+	final StructLiteral toStructLiteralFast() { return cast(StructLiteral)cast(void*)this; }
+	final UnionLiteral toUnionLiteralFast() { return cast(UnionLiteral)cast(void*)this; }
+	final ClassLiteral toClassLiteralFast() { return cast(ClassLiteral)cast(void*)this; }
+	final TypeExp toTypeExpFast() { return cast(TypeExp)cast(void*)this; }
+	final StoreExp toStoreExpFast() { return cast(StoreExp)cast(void*)this; }
+	final StatementExp toStatementExpFast() { return cast(StatementExp)cast(void*)this; }
+	final TokenExp toTokenExpFast() { return cast(TokenExp)cast(void*)this; }
+	final VaArgExp toVaArgExpFast() { return cast(VaArgExp)cast(void*)this; }
+	final PropertyExp toPropertyExpFast() { return cast(PropertyExp)cast(void*)this; }
+	final BuiltinExp toBuiltinExpFast() { return cast(BuiltinExp)cast(void*)this; }
+	final AccessExp toAccessExpFast() { return cast(AccessExp)cast(void*)this; }
+	final RunExp toRunExpFast() { return cast(RunExp)cast(void*)this; }
+
+	// Templates
+	final TemplateInstance toTemplateInstanceFast() { return cast(TemplateInstance)cast(void*)this; }
+	final TemplateDefinition toTemplateDefinitionFast() { return cast(TemplateDefinition)cast(void*)this; }
+
 
 private:
 	NodeType mNodeType;
