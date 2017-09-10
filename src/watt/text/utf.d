fdfacbe343da1fa938aca713162bd14209dea93e
Jakob Bornecrantz <wallbraker@gmail.com>
2017-09-10 12:23:35 +0200

    watt: Fix some warnings

--- src/watt/text/utf.d
+++ src/watt/text/utf.d
@@ -2,7 +2,8 @@ module watt.text.utf;
 
 import std.utf : decode, validate;
 private import std.conv : to;
-private import std.utf : toUTF8, utfencode = encode;
+private import std.utf : utfencode = encode;
+private import std.utf;
 
 string encode(dchar d) { return to!string(d); }
 string encode(dchar[] s) { return toUTF8(s); }
@@ -10,7 +11,6 @@ void encode(ref char[] s, dchar c) { utfencode(s, c); }
 
 void encode(void delegate(scope const(char)[]) sink, dchar c)
 {
-	char[] outString;
-	encode(outString, c);
-	sink(outString);
+	char[4] sbuf;
+	sink(sbuf[0 .. utfencode(sbuf, c)]);
 }
